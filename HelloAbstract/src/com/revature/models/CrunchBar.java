650ee73e0fba5e6e76dfd0774661cc104797ba35
benp3837 <petruzziellbj@alumni.vcu.edu>
2022-12-08 14:14:40 -0500

    almost done with HelloAbstract

--- HelloAbstract/src/com/revature/models/CrunchBar.java
+++ HelloAbstract/src/com/revature/models/CrunchBar.java
@@ -11,19 +11,38 @@ In other words, CrunchBar has inherited EVERY member found in Candy and in Food
 //"HEY. You need to either be an abstract Class, or implement those abstract methods you inherited"
 public class CrunchBar extends Candy {
 
+    //a couple variables unique to the CrunchBar Class
+    public int riceCount = 100;
+    public int brickCount = 15;
 
+    //This is the method from the Abstract Class. Every Candy can get opened, but not all in the same way.
     @Override
     public void getOpened() {
-        
+        System.out.println("CrunchBar got unwrapped");
     }
 
+    //This is one of the methods from the Interface. Not all Foods will get eaten the same way.
     @Override
     public void getEaten() {
-
+        System.out.println("CrunchBar gets broken apart and consumed");
     }
 
+    //This is another method from the Interface. Not all Foods get thrown away the same way.
     @Override
     public void throwAway(int days) {
+        System.out.println("CrunchBar got thrown away after " + days + " days. You monster.");
+    }
 
+    //constructors---------------------
+
+    //no-args constructor
+    public CrunchBar() {
+        super(); //this calls the no args constructor of Candy, which calls from the Object Class
+    }
+
+    //all-args
+    public CrunchBar(int riceCount, int brickCount) {
+        this.riceCount = riceCount;
+        this.brickCount = brickCount;
     }
 }
