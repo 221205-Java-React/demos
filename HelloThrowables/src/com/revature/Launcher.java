aeeae1fa9ec1630cfe53e1ae086442b41fafef8c
benp3837 <petruzziellbj@alumni.vcu.edu>
2022-12-09 11:26:19 -0500

    Finshed HelloThrowables

--- HelloThrowables/src/com/revature/Launcher.java
+++ HelloThrowables/src/com/revature/Launcher.java
@@ -1,8 +1,10 @@
 package com.revature;
 
+import com.revature.exceptions.MyCheckedException;
+
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyCheckedException {
 
         System.out.println("========================(Throwing Some Unchecked (Runtime) Exceptions)");
 
@@ -53,9 +55,39 @@ public class Launcher {
 
         System.out.println("=========================(Using the Custom Exception)");
 
+        throwCheckException();
+
+        //we can either put this in a try/catch block,
+        //OR make the main method also throw MyCheckedException
+
+        //if we went the try/catch route, we would simply wrap the method call in a try
+
     } //end of main method
 
-    
+    /*
+        Creating a method that throws MyCheckedException
+        methods that throw Checked Exceptions WILL NOT let you compile unless they're handled somehow
+        by saying this method "throws" MyCheckedException, we're handling it technically
+        more specifically, we're making it the problem of any method that calls this method
+    */
+    public static void throwCheckException() throws MyCheckedException{
+
+        System.out.println("I'm going to throw a Checked Exception!");
+
+        /*
+        The "throw" keyword is how we manually throw Exceptions
+        Exceptions are OBJECTS (see the "new" keyword), Exceptions are objects we throw to halt the app
+         */
+        throw new MyCheckedException("our custom Exception was thrown!!!");
+
+    }
+
+    /*
+    Two ways to handle exceptions (Dog Poop Analogy)
+
+     Try/Catch: You see dog poop on the floor and decide "ok let me handle this and clean it up"
+     Throws Keyword: I'm acknowledging this, but I'm gonna let someone else (another method) handle it
+     */
 
 
     //When the demo is done, we'll explore the debugger and breakpoints
