6509f52277a23ecf10abac4730cc7fedbbb7c954
benp3837 <petruzziellbj@alumni.vcu.edu>
2022-12-16 15:10:11 -0500

    login DAO method done

--- P1Demo/project1demo/src/main/java/com/revature/daos/AuthDAO.java
+++ P1Demo/project1demo/src/main/java/com/revature/daos/AuthDAO.java
@@ -1,22 +1,63 @@
 package com.revature.daos;
 
 import com.revature.models.Employee;
+import com.revature.models.Role;
 import com.revature.utils.ConnectionUtil;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AuthDAO {
 
     //for THIS method in particular, since we don't have username/password, we'll use first_name/last_name
-    //change it accordingly for your own application.
+    //change it accordingly for your own application. Users should have username/password in YOUR p1
 
-    public Employee login(String username, String password){
+    public Employee login(String first_name, String last_name){
 
         try(Connection conn = ConnectionUtil.getConnection()){
 
-        
+            String sql = "select * from employees where first_name = ? and last_name = ?;";
 
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, first_name);
+            ps.setString(2, last_name);
+
+            ResultSet rs = ps.executeQuery();
+
+            //since we're only expecting one record, we can just use an if with rs.next() instead of while
+            if(rs.next()){
+
+                Employee e = new Employee(
+                  rs.getInt("employee_id"),
+                  rs.getString("first_name"),
+                  rs.getString("last_name"),
+                  null
+                );
+
+                int roleFk = rs.getInt("role_id_fk");
+
+                RoleDAO rDAO = new RoleDAO();
+
+                Role r = rDAO.getRoleById(roleFk);
+
+                e.setRole(r);
+
+                //for comments on everything we're doing above, you can check getEmployees in EmployeeDAO
+
+                return e; //returning the Employee with the matching first_name/last_name
+
+            }
+
+
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+
+        return null;
 
     }
 
