2f48682d2d55a6ecfdb2c74cfcff2035689da02f
benp3837 <petruzziellbj@alumni.vcu.edu>
2022-12-19 14:36:43 -0500

    wrote some logs

--- Week3/HelloLogbackJunit/src/main/java/com/revature/Calculator.java
+++ Week3/HelloLogbackJunit/src/main/java/com/revature/Calculator.java
@@ -1,20 +1,30 @@
 package com.revature;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
 
+    //Instantiating a Logger object so that we can log our application
+    private static final Logger logger = LoggerFactory.getLogger(Calculator.class);
+
     public int sum(int x, int y){
+        logger.info("User added numbers " + x + " and " + y);
         return x + y;
     }
 
     public int difference(int x, int y){
+        logger.info("User subtracted numbers " + x + " and " + y);
         return x - y;
     }
 
     public int quotient(int x, int y){
         try{
+            logger.info("User divided numbers " + x + " and " + y);
             return x / y;
         }
         catch(ArithmeticException e){
+            logger.warn("User attempted to divide by zero!!");
             return 0; //not because it's actually zero, but as an error return placeholder.
         }
     }
--- Week3/HelloLogbackJunit/src/main/java/com/revature/Launcher.java
+++ Week3/HelloLogbackJunit/src/main/java/com/revature/Launcher.java
@@ -2,9 +2,13 @@ package com.revature;
 
 public class Launcher {
 
+
     public static void main(String[] args) {
 
+        //can't call a non-static member within a static method, UNLESS you make an object
+        Calculator calc = new Calculator();
 
+        calc.sum(54, 70);
 
     }
 
