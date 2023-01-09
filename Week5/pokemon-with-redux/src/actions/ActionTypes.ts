a60a2e4b603f2c547c749e4c1aeecc038edd40a6
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-09 10:57:36 -0500

    building the actions

---
+++ Week5/pokemon-with-redux/src/actions/ActionTypes.ts
@@ -0,0 +1,7 @@
+//We will store the different action types in this file. Typically different HTTP calls that will be made
+
+export const ADD_USER = 'ADD_USER' //hypothetical
+export const LOGIN_USER = 'LOGIN_USER' //won't implement
+
+export const ADD_POKEMON = 'ADD_POKEMON' //hypothetical
+export const GET_POKEMON = 'GET_POKEMON' //will implement
---
+++ Week5/pokemon-with-redux/src/store/store.ts
@@ -0,0 +1,29 @@
+//This is out actual store file, which will store all the active data in our application
+
+import { configureStore } from "@reduxjs/toolkit";
+import { AppState } from "./types";
+
+
+/* The initial state of the application in the store will have a default user and pokemon.
+    We contain the state in a globally visible object (visible to the ENTIRE application) 
+    When a user or pokemon payload (data) get dispatched (sent) here, this state will change */
+const state:AppState = {
+    user: {
+        id:0,
+        username:"",
+        password:""
+    },
+    poke: {
+        id:0,
+        name:"",
+        sprite:""
+    }
+}
+/* These default values will get changed as the user uses the application
+When a user logs in, this user object will get populated with their data (won't do this one in the demo) When we GET a pokemon, this poke object will get populated (we WILL see this one) */
+
+/* here, I'm just configuring our store to be able to use reducers so that we can dispatch new data
+so when store data changes, reducers AUTOMATICALLY notify components, thus changing the user's view */
+export const store = configureStore({
+    reducer
+})
