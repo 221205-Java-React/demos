42bfa729f211dd4891ade25ac0cd2cb7abd0d87c
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-10 11:33:18 -0500

    Account class

--- Week6/HelloSpring/src/main/java/com/revature/models/Account.java
+++ Week6/HelloSpring/src/main/java/com/revature/models/Account.java
@@ -5,6 +5,38 @@ public class Account {
     private double accountBalance;
     private String accountType;
 
-    
+    //boilerplate code--------------------------------
 
+    public Account() {
+    }
+
+    public Account(double accountBalance, String accountType) {
+        this.accountBalance = accountBalance;
+        this.accountType = accountType;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountBalance=" + accountBalance +
+                ", accountType='" + accountType + '\'' +
+                '}';
+    }
+    
 }
