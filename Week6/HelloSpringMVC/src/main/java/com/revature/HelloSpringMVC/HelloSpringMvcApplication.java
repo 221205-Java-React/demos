29726db64fa8e9e8f739e0abdbf25f4d626d2000
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-10 15:17:28 -0500

    started HelloSpringMVC - just setup

---
+++ Week6/HelloSpringMVC/src/main/java/com/revature/HelloSpringMVC/HelloSpringMvcApplication.java
@@ -0,0 +1,19 @@
+package com.revature.HelloSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.revature") //we need this to scan for Spring Beans
+public class HelloSpringMvcApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloSpringMvcApplication.class, args);
+
+
+
+	}
+
+}
