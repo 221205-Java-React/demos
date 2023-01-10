50da302594087a46475a0e37149df4bc7be236f5
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-10 11:37:55 -0500

    User class

--- Week6/HelloSpring/src/main/java/com/revature/models/Account.java
+++ Week6/HelloSpring/src/main/java/com/revature/models/Account.java
@@ -38,5 +38,5 @@ public class Account {
                 ", accountType='" + accountType + '\'' +
                 '}';
     }
-    
+
 }
--- Week6/HelloSpring/src/main/java/com/revature/models/User.java
+++ Week6/HelloSpring/src/main/java/com/revature/models/User.java
@@ -2,6 +2,51 @@ package com.revature.models;
 
 public class User {
 
+    private int userId;
+    private String userName;
+    private Account account;
 
+    //boilerplate code-------------------------
+    
+    public User() {
+    }
 
+    public User(int userId, String userName, Account account) {
+        this.userId = userId;
+        this.userName = userName;
+        this.account = account;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", account=" + account +
+                '}';
+    }
 }
