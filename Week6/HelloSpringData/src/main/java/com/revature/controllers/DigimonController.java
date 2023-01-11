fe5278a6c9dda2d8cd00ae39afb3c17334339437
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-11 14:50:53 -0500

    custom DAO method in HelloSpringData

--- Week6/HelloSpringData/src/main/java/com/revature/controllers/DigimonController.java
+++ Week6/HelloSpringData/src/main/java/com/revature/controllers/DigimonController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController //This combines @Controller and @ResponseBody
 @CrossOrigin //lets us take in HTTP requests (might have to specify ports in your P2)
 @RequestMapping(value="/digimon")
@@ -40,7 +43,41 @@ public class DigimonController {
 
         //if insert succeeded...
         return ResponseEntity.accepted().body(newDigimon); //send a 202 status code and the new digimon
+    }
+
+    //GET all digimon - any GET requests to /digimon will go here
+    @GetMapping
+    public ResponseEntity<List<Digimon>> getAllDigimon(){
+
+        return ResponseEntity.ok(dDAO.findAll()); //.ok() returns a 200 level status code
+
+        //note that I'm nesting the DB call in the actual return
+        //this is shorthand - I could have done .ok().body(dDAO.findAll());
 
     }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Digimon> findById(@PathVariable int id){
+
+        /* findById from JpaRepository returns an Optional
+          Optionals lend to code flexibility because it MAY OR MAY NOT have the request object.
+          This helps us avoid NullPointerExceptions
+         */
+        Optional<Digimon> digimonOptional = dDAO.findById(id);
+
+        //we can check if the optional has data with .isPresent(), or .isEmpty()
+        if(digimonOptional.isPresent()){
+            //we can extract the Optional's data with .get()
+            Digimon extractedDigimon = digimonOptional.get();
+
+            return ResponseEntity.ok(extractedDigimon);
+        }
+
+        //if get by ID failed...
+        return ResponseEntity.badRequest().build(); //returning a 400 with no response body
+
+    }
+
+
 
 }
--- Week6/HelloSpringData/src/main/java/com/revature/daos/DigimonDAO.java
+++ Week6/HelloSpringData/src/main/java/com/revature/daos/DigimonDAO.java
@@ -4,6 +4,9 @@ import com.revature.models.Digimon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /*By extending JpaRepository, we get access to various DAO methods that we don't need to create
   try to ctrl + click on JpaRepository to see what we have provided for us*/
 
@@ -13,5 +16,17 @@ import org.springframework.stereotype.Repository;
 public interface DigimonDAO extends JpaRepository<Digimon, Integer> {
 
     //This is our entire DAO for now... showing custom DAO method below
+
+    //I want to find all digimon of a certain name
+    //Unfortunately, we have to define this on our own because JpaRepository only knows the Primary Key^
+    //BUT Spring Data is smart enough to implement this method for us
+    public Optional<List<Digimon>> findByDigimonName(String name);
+
+    //HOW DOES THIS WORK?
+    //By having a method name starting with "findBy" and ending in the variable you want to find by!!
+    //Spring needs your method name to be in camelCase, or it will throw vague errors
+    //Your variable names cannot have underscores
+
+    //there are a lot of options for custom methods, google it if you wish.
 
 }
