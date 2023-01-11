6146f91c473757bb8a9e34eb4d1157aed5dcfa10
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-11 11:43:19 -0500

    showed JPA/ORM HelloSpringData

---
+++ Week6/HelloSpringData/src/main/java/com/revature/HelloSpringData/HelloSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.revature.HelloSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringDataApplication.class, args);
+	}
+
+}
---
+++ Week6/HelloSpringData/src/test/java/com/revature/HelloSpringData/HelloSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.HelloSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
