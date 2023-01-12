d098ec64ea0233f808bcd9cfb316f08ce0e7e136
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-12 12:38:05 -0500

    saw @Before advice with within pointcut in HelloSpringAOP

--- Week6/HelloSpringAOP/src/main/java/com/revature/HelloSpringAOP/HelloSpringAopApplication.java
+++ Week6/HelloSpringAOP/src/main/java/com/revature/HelloSpringAOP/HelloSpringAopApplication.java
@@ -1,5 +1,7 @@
 package com.revature.HelloSpringAOP;
 
+import com.revature.daos.AvengerDAO;
+import com.revature.models.Avenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +19,29 @@ public class HelloSpringAopApplication {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//we are instantiating a new ApplicationContext to hold our Spring Beans
 		//in order to configure it, we need to tell it where our applicationContext.xml is.
+
+		//Instantiate an AvengerDAO
+		AvengerDAO aDAO = ac.getBean(AvengerDAO.class);
+
+		//Instantiate an Avenger object with the getById DAO method
+		Avenger a = aDAO.getById(4);
+
+		//this isn't making any logs^^ because technically no Avenger methods ran. we just grabbed one
+
+		//In order for any logs to occur, we need an Avenger BEAN
+		Avenger a2 = ac.getBean(Avenger.class);
+
+		//call some setter methods to initialize the fields of this bean and see some logs
+		a2.setAveId(10);
+		a2.setAveName("Static Shock");
+		a2.setFirstName("Virgil");
+		a2.setLastName("Hawkins");
+		a2.setPower("Static Shock");
+		a2.setPowerLevel(2352);
+
+		//show after returning/after throwing log
+
+		//and maybe around
 
 	}
 
--- Week6/HelloSpringAOP/src/main/java/com/revature/aspects/LoggingAspect.java
+++ Week6/HelloSpringAOP/src/main/java/com/revature/aspects/LoggingAspect.java
@@ -1,4 +1,42 @@
 package com.revature.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect //This Class is an ASPECT. We will handle all of our Logging in this one Class
 public class LoggingAspect {
+
+    //a Logger object so that we can do logging
+    private static Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    //In an Aspect, methods are called ADVICES.
+    //Each advice will be responsible for logging certain parts of the application
+
+    /*This advice will create a log BEFORE any method found in the models package executes*/
+    @Before("within(com.revature.models.*)")
+    public void logModelMethods(JoinPoint jp){
+
+        //getTarget() returns the class/object in question
+        //getSignature() returns the method signature
+        log.info(jp.getTarget() + " invoked " + jp.getSignature());
+
+    }
+
+    /*This advice will run AFTER successfully returning a value
+    A pointcut is where advice WILL be executed
+    In this case, the pointcut is any method called fight that returns a String and takes any parameters)
+    the "returning" attribute is simply giving us access to the object that gets returned from fight()
+      */
+    @AfterReturning(pointcut="execution(String fight(..))", returning="returnedObject")
+    public void logSuccessfulMethod(JoinPoint jp, Object returnedObject){
+
+        log.info(jp.getTarget() + " invoked " + jp.getSignature() + " returning " + returnedObject);
+    }
+
 }
